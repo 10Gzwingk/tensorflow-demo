25501916d773b5cb4937b3d2d82e620af9c7582d
ZhuHC <ZhuHC-Mac@accelerator.ink>
2023-02-15 14:23:17 +0800

    feat: active

--- RegionDemo.py
+++ RegionDemo.py
@@ -2,15 +2,24 @@ import region as reg
 import numpy as np
 
 #
+# regionA = reg.Region(100)
+# regionB = reg.Region(100)
+# regionC = reg.Region(100)
+# regionA.join_cross(regionB.output_v)
+# regionB.join_cross(regionC.output_v)
+# regionC.join_cross(regionA.output_v)
+# regionA.iterate()
+# regionB.iterate()
+# regionC.iterate()
+# regionA.iterate()
+# print(regionA.output_v)
+np.set_printoptions(linewidth=2000)
 regionA = reg.Region(100)
-regionB = reg.Region(100)
-regionC = reg.Region(100)
-regionA.join_cross(regionB.output_v)
-regionB.join_cross(regionC.output_v)
-regionC.join_cross(regionA.output_v)
-regionA.iterate()
-regionB.iterate()
-regionC.iterate()
-regionA.iterate()
-print(regionA.output_v)
+for i in range(500):
+    regionA.iterate()
+    regionA.output_v[0, 0] = 1
+    print(np.transpose(regionA.output_v))
+for k in range(500):
+    regionA.iterate()
+    print(np.transpose(regionA.output_v))
 
--- region.py
+++ region.py
@@ -14,13 +14,11 @@ class Region:
     def __init__(self, size):
         self.cross_list = []
         self.size = size
-        self.fc_net = random((size, size))  # 当前 region 的全连接矩阵
-        self.step = random((size, size))    # 神经元记忆步长
-        self.base = random((size, size))    # 神经元记忆偏置，输入信号大于 base 的才进行强化，小于 base 的则弱化
+        self.fc_net = random((size, size + 1))  # 当前 region 的全连接矩阵
+        self.step = random((size, size + 1))    # 神经元记忆步长
+        self.base = random((size, size + 1))    # 神经元记忆偏置，输入信号大于 base 的才进行强化，小于 base 的则弱化
         self.output_v = random((size, 1))   # 当前 region 的神经元输出向量，维度不可变
-        self.offset_v = random((size, 1))
-        self.offset_base = random((size, 1))
-        self.offset_step = random((size, 1))
+        self.fc_net[:, size] = random((size, 1))[:, 0] * size
 
     def join_cross(self, v):
         self.cross_list.append(v)
@@ -38,21 +36,15 @@ class Region:
     def iterate(self):
         # 组装其他 region 的输入向量
         # 根据接入的神经元的最新输出组装出本次迭代的输入
-        vector = self.output_v
+        vector = np.vstack([self.output_v, np.negative(np.ones([1, 1]))])
         for cross_v_item in self.cross_list:
             vector = np.vstack([vector, cross_v_item])
 
-        # 原始输出，由二次非归一，转为线性归一化
-        output = self.fc_net.dot(vector) - 1
-        output = output / np.power(self.size, 0.5)
-        output_v = np.minimum(output, np.ones((self.size, 1)))
-
-        # 激活
-        sign = output_v / np.abs(output_v)
-        output_v = np.abs(output_v)
-        output_v = np.maximum(output_v - self.offset_v, 0)
+        # 激活,原始输出，TODO 由二次非归一，转为线性归一化
+        output_v = self.fc_net.dot(vector) / self.size
+        output_v = np.maximum(output_v, 0)
         output_v = np.minimum(output_v, 1)
-        output_v = output_v * sign
+        output_v = np.power(output_v, 0.5)
 
         if output_v.shape != self.output_v.shape:
             print("[error] shape mismatch")
@@ -61,14 +53,9 @@ class Region:
         self.output_v = output_v
 
         # 基于本次的输入更新权重
-        self.fc_net = np.arctan(
-            (np.ones((self.size, 1)) * np.transpose(vector) - self.base) * self.step + np.tan(self.fc_net * math.pi / 2)
-        ) / (math.pi / 2)
-        # 根据输出更新偏移
-        # self.offset_v = self.update(self.offset_v, self.output_v)
-        self.fc_net = np.arctan(
-            (self.output_v - self.base) * self.offset_step + np.tan(self.offset_v * math.pi / 2)
-        ) / (math.pi / 2)
+        temp = np.ones((self.size, 1)) * np.transpose(vector)
+        temp[:, self.size] = output_v[:, 0]
+        self.fc_net = np.arctan((temp - self.base) * self.step + np.tan(self.fc_net * math.pi / 2)) / (math.pi / 2)
 
     def update(self, x, v):
         return np.arctan((np.ones((self.size, 1)) * np.transpose(v) - self.base) * self.step + np.tan(x * math.pi / 2)) / (math.pi / 2)
