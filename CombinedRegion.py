a033081a34af2e6ed5ea21e985e0f73903ef16eb
ZhuHC <zhc426842@qq.com>
2023-02-27 00:29:41 +0800

    feat: combined region

---
+++ CombinedRegion.py
@@ -0,0 +1,18 @@
+from region import Region
+
+
+class CombinedRegion(Region):
+    def __init__(self, regions):
+        self.regions = regions
+
+    def join_cross(self, v, join_type):
+        return
+
+    def exit_cross(self, v):
+        return
+
+    def iterate(self):
+        return
+
+    def update(self, x, v):
+        return
--- region.py
+++ region.py
@@ -6,8 +6,10 @@ class Region:
     def exit_cross(self, v):
         raise NotImplementedError
 
+    # 网络迭代
     def iterate(self):
         raise NotImplementedError
 
+    # 更新权重
     def update(self, x, v):
         raise NotImplementedError
